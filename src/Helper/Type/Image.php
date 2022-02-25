a35276538c3a8da43d6ebb03d63319da218372a5
Kuanysh <developerandroidkz@gmail.com>
2022-02-25 22:12:02 +0500

    fix bug

--- src/Helper/Type/Image.php
+++ src/Helper/Type/Image.php
@@ -2,9 +2,8 @@
 declare(strict_types=1);
 
 namespace App\Helper\Type;
-use App\Helper\Type;
 
-class Image extends Type{
+class Image{
 
     /**
      * @var array<mixed>
@@ -19,6 +18,25 @@ class Image extends Type{
         "hint" => null,
         "value" => null
     );
+
+    public static $_instance = null;
+
+    public static function getInstance ()
+    {
+        if (self::$_instance === null) {
+            self::$_instance = new self;
+        }
+
+        return self::$_instance;
+    }
+
+	public function __call($method, $args) :self{
+		if (!in_array($method, array_keys($this->array))) {
+			throw new DomainException("Method not found");
+		}
+        $this->array[$method] = $args[0];
+		return $this;
+	}
 
     public function execute() :array {
         return $this->array;
