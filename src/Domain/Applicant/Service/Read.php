1f69b217d4e2ca9d2beb346ed7341afa415b2a40
Kuanysh <developerandroidkz@gmail.com>
2022-02-27 23:26:40 +0500

    test

--- src/Domain/Applicant/Service/Read.php
+++ src/Domain/Applicant/Service/Read.php
@@ -64,13 +64,13 @@ final class Read{
      */
     public static function getHeader() :array{
         return array(
-            "raiting_number" => Field::getInstance()->init(new Number())->execute(),
+            "raiting_number" => Field::getInstance()->init(new Number())->is_visible(false)->execute(),
             "iin" => Field::getInstance()->init(new Number())->can_create(true)->is_required(true)->min_length(12)->max_length(12)->execute(),
             "full_name" => Field::getInstance()->init(new Text())->can_create(true)->can_update(true)->is_required(true)->min_length(3)->execute(),
             "birthdate" => Field::getInstance()->init(new Date())->can_create(true)->is_required(true)->min_date("1900-01-01")->max_date(date("Y-m-d"))->execute(),
             "email" => Field::getInstance()->init(new Email())->can_update(true)->execute(),
             "phone_number" => Field::getInstance()->init(new PhoneNumber())->is_required(true)->can_update(true)->execute(),
-            "address" => Field::getInstance()->init(new Textarea())->can_update(true)->is_required(true)->execute(),
+            "address" => Field::getInstance()->init(new Textarea())->can_create(true)->can_update(true)->is_required(true)->execute(),
             "second_phone_number" => Field::getInstance()->init(new Textarea())->can_update(true)->execute(),
             "is_have_whatsapp" => Field::getInstance()->init(new Boolean())->execute(),
             "is_have_telegram" => Field::getInstance()->init(new Boolean())->execute(),
