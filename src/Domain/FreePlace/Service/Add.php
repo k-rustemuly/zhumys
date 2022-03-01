35402e47a1c04c755700d380423f71ea5878d29f
Kuanysh <developerandroidkz@gmail.com>
2022-03-01 15:34:06 +0500

    deleted unused classes

--- src/Domain/FreePlace/Service/Add.php
+++ src/Domain/FreePlace/Service/Add.php
@@ -4,47 +4,18 @@ namespace App\Domain\FreePlace\Service;
 
 use App\Domain\Company\Admin;
 use App\Domain\FreePlace\Repository\FreePlaceCreaterRepository;
-use App\Domain\FreePlace\Repository\FreePlaceReadRepository;
-use App\Domain\FreePlace\Log\Repository\LogCreatorRepository;
-use App\Domain\FreePlace\Repository\FreePlaceDeleterRepository;
 use DomainException;
-use App\Helper\Pki;
 use App\Helper\Validator;
-use Spatie\ArrayToXml\ArrayToXml;
 
 /**
  * Service.
  */
 final class Add extends Admin{
-    /**
-     * @var int
-     */
-    private $admin_type_id = 2;
 
     /**
      * @var FreePlaceCreaterRepository
      */
     private $createRepository;
-
-    /**
-     * @var LogCreatorRepository
-     */
-    private $logCreateRepository;
-
-    /**
-     * @var FreePlaceReadRepository
-     */
-    private $readRepository;
-
-    /**
-     * @var FreePlaceDeleterRepository
-     */
-    private $deleteRepository;
-
-    /**
-     * @var Pki
-     */
-    private $pki;
 
     /**
      * @var Validator
@@ -54,22 +25,10 @@ final class Add extends Admin{
     /**
      * The constructor.
      * @param FreePlaceCreaterRepository $createRepository
-     * @param FreePlaceReadRepository $readRepository
-     * @param LogCreatorRepository $logCreateRepository
-     * @param FreePlaceDeleterRepository $deleteRepository
-     * @param Pki               $pki The pki client
      *
      */
-    public function __construct(FreePlaceCreaterRepository $createRepository,
-                                Pki $pki,
-                                FreePlaceReadRepository $readRepository,
-                                LogCreatorRepository $logCreateRepository,
-                                FreePlaceDeleterRepository $deleteRepository) {
+    public function __construct(FreePlaceCreaterRepository $createRepository) {
         $this->createRepository = $createRepository;
-        $this->pki = $pki;
-        $this->readRepository = $readRepository;
-        $this->logCreateRepository = $logCreateRepository;
-        $this->deleteRepository = $deleteRepository;
         $this->validator = new Validator();
     }
 
