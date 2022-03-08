b41135703d2f932ed5dbdb549f9c7e7419fce8ee
Kuanysh <developerandroidkz@gmail.com>
2022-03-08 23:23:39 +0500

    added free place ranging info for company

--- src/Domain/FreePlace/Service/About.php
+++ src/Domain/FreePlace/Service/About.php
@@ -13,6 +13,8 @@ use App\Helper\Fields\DateTime;
 use App\Domain\Company\Admin;
 use App\Helper\Fields\Text;
 use DomainException;
+use App\Domain\Ranging\Repository\RangingReaderRepository;
+
 /**
  * Service.
  */
@@ -39,14 +41,23 @@ final class About extends Admin{
     private $freePlaceInfo;
 
     /**
+     * @var RangingReaderRepository
+     */
+    private $rangingReadRepository;
+
+    /**
      * The constructor.
      * @param FreePlaceReadRepository $readRepository
      * @param LogReadRepository $logReadRepository
+     * @param RangingReaderRepository $rangingReadRepository
      *
      */
-    public function __construct(FreePlaceReadRepository $readRepository, LogReadRepository $logReadRepository) {
+    public function __construct(FreePlaceReadRepository $readRepository,
+                                LogReadRepository $logReadRepository,
+                                RangingReaderRepository $rangingReadRepository) {
         $this->readRepository = $readRepository;
         $this->logReadRepository = $logReadRepository;
+        $this->rangingReadRepository = $rangingReadRepository;
         $this->render = new Render();
     }
 
@@ -62,11 +73,14 @@ final class About extends Admin{
     public function get(int $id, string $lang) :array{
         $this->freePlaceInfo = $this->readRepository->findByBinAndIdAndLang($this->getBin(), $id, $lang);
         if(empty($this->freePlaceInfo)) throw new DomainException("Free place not found");
-        return $this->render
-        ->lang($lang)
-        ->block("free_place_info", $this->getFreePlaceBlockValues())
-        ->block("free_place_log_info", $this->getFreePlaceLogBlockValues($this->logReadRepository->getAllByIdAndLang($id, $lang)))
-        ->build();
+        $render = $this->render
+                ->lang($lang)
+                ->block("free_place_info", $this->getFreePlaceBlockValues())
+                ->block("free_place_log_info", $this->getFreePlaceLogBlockValues($this->logReadRepository->getAllByIdAndLang($id, $lang)));
+        if($this->freePlaceInfo["status_id"] == 5) {
+            $render = $render->block("free_place_ranging_info", $this->getFreePlaceRangingBlockValues($lang, $this->rangingReadRepository->getAllByFreePlaceIdAndLang($id, $lang)), "table");
+        }        
+        return $render->build();
     }
 
     /**
@@ -81,7 +95,7 @@ final class About extends Admin{
             "position_id" => Field::getInstance()->init(new Reference())->reference_name("position")->reference_id("id")->value(array("id" => $this->freePlaceInfo["position_id"], "value" => $this->freePlaceInfo["position_name"]))->can_update(true)->execute(),
             "count" => Field::getInstance()->init(new Number())->is_required(true)->value($this->freePlaceInfo["count"])->can_update(true)->execute(),
             "comment" => Field::getInstance()->init(new Textarea())->value($this->freePlaceInfo["comment"])->can_update(true)->execute(),
-            "status_id" => Field::getInstance()->init(new Reference())->reference_name("place-status")->reference_id("id")->value(array("id" => $this->freePlaceInfo["status_id"], "value" => $this->freePlaceInfo["status_name"]))->execute(),
+            "status_id" => Field::getInstance()->init(new Reference())->reference_name("place-status")->reference_id("id")->value(array("id" => $this->freePlaceInfo["status_id"], "value" => $this->freePlaceInfo["status_name"], "color" => $this->freePlaceInfo["status_color"]))->execute(),
             "reason" => Field::getInstance()->init(new Text())->value($this->freePlaceInfo["reason"])->execute(),
             "created_at" => Field::getInstance()->init(new DateTime())->value($this->freePlaceInfo["created_at"])->execute(),
         );
@@ -98,7 +112,7 @@ final class About extends Admin{
         foreach ($data as $i => $v){
             $array[$i] = array(
                 "admin_type_id" => Field::getInstance()->init(new Reference())->reference_name("admin")->reference_id("id")->value(array("id" => $v["admin_type_id"], "value" => $v["admin_type_name"]))->execute(),
-                "status_id" => Field::getInstance()->init(new Reference())->reference_name("place-status")->reference_id("id")->value(array("id" => $v["status_id"], "value" => $v["status_name"]))->execute(),
+                "status_id" => Field::getInstance()->init(new Reference())->reference_name("place-status")->reference_id("id")->value(array("id" => $v["status_id"], "value" => $v["status_name"], "color" => $v["status_color"]))->execute(),
                 "admin_full_name" => Field::getInstance()->init(new Text())->value($v["admin_full_name"])->execute(),
                 "company_name" => Field::getInstance()->init(new Text())->value($v["company_name"])->execute(),
                 "reason" => Field::getInstance()->init(new Text())->value($v["reason"])->execute(),
@@ -106,6 +120,36 @@ final class About extends Admin{
             );
         }
         return $array;
+    }
+
+    /**
+     * Get data for block free place ranging result
+     *
+     * @param string $lang
+     * @param array<mixed> $data
+     * 
+     * @return array<mixed>
+     */
+    private function getFreePlaceRangingBlockValues(string $lang, array $data) :array {
+        foreach ($data as $i => $v) {
+            $data[$i]["status_id"] = array("id" => $v["status_id"], "value" => $v["status_name"], "color" => $v["status_color"]);
+            $data[$i]["privilege_id"] = array("id" => $v["privilege_id"], "value" => $v["privilege_name"]);
+            unset($data[$i]["status_name"]);
+            unset($data[$i]["status_color"]);
+            unset($data[$i]["privilege_name"]);
+        }
+        $render = new Render();
+        $render->lang($lang)
+                ->header(
+                    array(
+                        "raiting_number" => Field::getInstance()->init(new Number())->execute(),
+                        "iin" => Field::getInstance()->init(new Number())->execute(),
+                        "full_name" => Field::getInstance()->init(new Text())->execute(),
+                        "privilege_id" => Field::getInstance()->init(new Reference())->reference_name("privilege")->reference_id("id")->execute(),
+                        "status_id" => Field::getInstance()->init(new Reference())->reference_name("ranging-status")->reference_id("id")->execute(),
+                    )
+                )->data($data);
+        return $render->build();
     }
 
 }
