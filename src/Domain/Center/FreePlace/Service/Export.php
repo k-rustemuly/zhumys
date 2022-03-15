3a893ad5b860169d31668a7dc0a9ee2c41c89480
Asset1996 <khaitbay96@gmail.com>
2022-03-15 17:47:07 +0500

    Third commit

--- src/Domain/Center/FreePlace/Service/Export.php
+++ src/Domain/Center/FreePlace/Service/Export.php
@@ -13,6 +13,35 @@ class Export {
      */
     private $render;
 
+    private $cells = [
+        0 => 'A',
+        1 => 'B',
+        2 => 'C',
+        3 => 'D',
+        4 => 'E',
+        5 => 'F',
+        6 => 'G',
+        7 => 'H',
+        8 => 'I',
+        9 => 'J',
+        10 => 'K',
+        11 => 'L',
+        12 => 'M',
+        13 => 'N',
+        14 => 'O',
+        15 => 'P',
+        16 => 'Q',
+        18 => 'R',
+        18 => 'S',
+        19 => 'T',
+        20 => 'U',
+        21 => 'V',
+        22 => 'W',
+        23 => 'X',
+        24 => 'Y',
+        25 => 'Z',
+    ];
+
     /**
      * The constructor.
      */
@@ -43,14 +72,63 @@ class Export {
             $data["data"][$k] = array_merge($header['header'], $v);
         }
 
+        $headerStyleArray = [
+            'font' => [
+                'bold' => true,
+            ],
+            'borders' => [
+                'top' => [
+                    'borderStyle' => \PhpOffice\PhpSpreadsheet\Style\Border::BORDER_THICK,
+                ],
+                'right' => [
+                    'borderStyle' => \PhpOffice\PhpSpreadsheet\Style\Border::BORDER_THICK,
+                ],
+                'bottom' => [
+                    'borderStyle' => \PhpOffice\PhpSpreadsheet\Style\Border::BORDER_THICK,
+                ],
+                'left' => [
+                    'borderStyle' => \PhpOffice\PhpSpreadsheet\Style\Border::BORDER_THICK,
+                ],
+            ],
+            'fill' => [
+                'fillType' => \PhpOffice\PhpSpreadsheet\Style\Fill::FILL_SOLID,
+                'color' => [
+                    'argb' => '808080',
+                ],
+            ]
+        ];
+
+        $dataStyleArray = [
+            'font' => [
+                'bold' => false,
+            ],
+            'borders' => [
+                'top' => [
+                    'borderStyle' => \PhpOffice\PhpSpreadsheet\Style\Border::BORDER_MEDIUM,
+                ],
+                'right' => [
+                    'borderStyle' => \PhpOffice\PhpSpreadsheet\Style\Border::BORDER_MEDIUM,
+                ],
+                'bottom' => [
+                    'borderStyle' => \PhpOffice\PhpSpreadsheet\Style\Border::BORDER_MEDIUM,
+                ],
+                'left' => [
+                    'borderStyle' => \PhpOffice\PhpSpreadsheet\Style\Border::BORDER_MEDIUM,
+                ],
+            ]
+        ];
+
         $spreadsheet = new Spreadsheet();
         $sheet = $spreadsheet->getActiveSheet();
+        $sheet->getDefaultColumnDimension()->setWidth(150, 'pt');
         $sheet->setCellValue('A1', 'No');
         $sheet->fromArray([array_values($header['header'])], Null, 'B1');
+        $sheet->getStyle('A1:' . (string)( $this->cells[count($header['header'])] ) . '1')->applyFromArray($headerStyleArray);
 
         for ($i=1; $i <= count($data['data']); $i++) { 
             $sheet->setCellValue('A'.(string)($i+1), (string)($i));
             $sheet->fromArray([array_values($data['data'][$i-1])], Null, 'B'.(string)($i+1));
+            $sheet->getStyle('A'.(string)($i+1).':' . (string)( $this->cells[count($header['header'])] ) . (string)( count($header['header']) ))->applyFromArray($dataStyleArray);
         }
 
         return $spreadsheet;
