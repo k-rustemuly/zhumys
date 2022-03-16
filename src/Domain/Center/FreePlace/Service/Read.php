13c39cd6b69bdd63778217870f9c6be83371e35b
Kuanysh <developerandroidkz@gmail.com>
2022-03-16 16:14:38 +0500

    fix bug

--- src/Domain/Center/FreePlace/Service/Read.php
+++ src/Domain/Center/FreePlace/Service/Read.php
@@ -70,11 +70,12 @@ final class Read {
      */
     public static function getHeader() :array{
         return array(
-            "id" => Field::getInstance()->init(new Number())->execute(),
+            //"id" => Field::getInstance()->init(new Number())->is_visible(false)->execute(),
             "bin" => Field::getInstance()->init(new Number())->can_create(true)->min_length(12)->max_length(12)->execute(),
             "company_name" => Field::getInstance()->init(new Text())->execute(),
             "position_id" => Field::getInstance()->init(new Reference())->can_create(true)->can_update(true)->reference_name("position")->reference_id("id")->is_required(true)->execute(),
-            "count" => Field::getInstance()->init(new Number())->can_create(true)->can_update(true)->is_required(true)->min(1)->execute(),
+            "free_place_count" => Field::getInstance()->init(new Number())->can_create(true)->can_update(true)->is_required(true)->min(1)->execute(),
+            "employed_count" => Field::getInstance()->init(new Number())->execute(),
             //"comment" => Field::getInstance()->init(new Textarea())->can_create(true)->can_update(true)->is_visible(false)->execute(),
             "status_id" => Field::getInstance()->init(new Reference())->reference_name("place-status")->reference_id("id")->execute(),
             "created_at" => Field::getInstance()->init(new DateTime())->execute(),
@@ -102,6 +103,7 @@ final class Read {
                         unset($data[$i]["status_name"]);
                         unset($data[$i]["status_color"]);
                     break;
+
                 }
             }
         }
